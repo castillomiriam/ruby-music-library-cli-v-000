2edb4e17e1041d1917fa7a960bf9f0161e43428b
Miriam Castillo <castillo.m.mc@gmail.com>
2018-12-04 02:55:37 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|artist| artist.name == name}
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|artist| artist.name == name}
+  end
+
+end
